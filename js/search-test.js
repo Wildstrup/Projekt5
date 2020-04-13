90a97969339bd931224ef12cbd529ae9914b05b1
Pro-Feht <60446648+Pro-Feht@users.noreply.github.com>
2020-04-13 02:36:13 +0200

    Kort pause

--- js/search-test.js
+++ js/search-test.js
@@ -1,11 +1,11 @@
 const Butikker = [
-  {name: 'Odense'},
-  {name: 'Slagelse'},
-  {name: 'Vejle'},
-  {name: 'Ringsted'},
-  {name: 'Næstved'},
-  {name: 'København'},
-  {name: 'Randers'},
+  {name: 'odense'},
+  {name: 'slagelse'},
+  {name: 'vejle'},
+  {name: 'ringsted'},
+  {name: 'næstved'},
+  {name: 'kbenhavn'},
+  {name: 'randers'},
 ];
 
 const list = document.getElementById('list');
@@ -25,15 +25,38 @@ function setlist(group) {
 }
 
 function clearlist() {
-
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
 }
 
 function setNoResults(){
+  const item = document.createElement('li');
+  item.classList.add('list-group-item');
+  const text = document.createTextNode('Intet resultat fundet');
+  item.appendChild(text);
+  list.appendChild(item);
+}
 
+function getRelevancy(value, seachTerm){
+    if (value === seachTerm) {
+      return 2;
+    } else if (value.startWith(term)) {
+      return 1;
+
+    }
 }
 
 const searchInput = document.getElementById('search');
 
 searchInput.addEventListener('input', () => {
   const value = event.target.value;
+  if (value && value.trim().length > 0) {
+      value = value.trim().toLowerCase();
+      setList(Butikker.filter(butik => {
+        return butik.name.includes(value);
+      }));
+  } else {
+    clearlist();
+  }
 });
