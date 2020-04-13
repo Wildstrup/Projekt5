1eadb3893a7032d77b55176994bec46293997a89
Pro-Feht <60446648+Pro-Feht@users.noreply.github.com>
2020-04-13 02:22:14 +0200

    Vi fortsætter kampen imod søgefeltet!

--- js/search-test.js
+++ js/search-test.js
@@ -5,10 +5,35 @@ const Butikker = [
   {name: 'Ringsted'},
   {name: 'Næstved'},
   {name: 'København'},
+  {name: 'Randers'},
 ];
+
+const list = document.getElementById('list');
+
+function setlist(group) {
+  clearlist();
+  for (const Butikker of group){
+    const item = document.createElement('li');
+    item.classList.add('list-group-item');
+    const text = document.createTextNode(Butikker.name);
+    item.appendChild(text);
+    list.appendChild(item);
+  }
+  if (group.length === 0){
+    setNoResults();
+  }
+}
+
+function clearlist() {
+
+}
+
+function setNoResults(){
+
+}
 
 const searchInput = document.getElementById('search');
 
 searchInput.addEventListener('input', () => {
-  console.log('input event fired');
+  const value = event.target.value;
 });
