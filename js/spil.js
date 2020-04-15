e79c7ef67744cec1fc9fff56df8b3feb3fa87ec5
Kumaru2 <60379752+Kumaru2@users.noreply.github.com>
2020-04-15 12:36:45 +0200

    game js
    jaja kort spil

--- js/spil.js
+++ js/spil.js
@@ -3,8 +3,8 @@ class MixorMatch{
     this.cardsArray = cards;
     this.totalTime = totalTime;
     this.timeRemaining = totalTime;
-    this.timer = document.getElementsById('time-remaining');
-    this.ticker = document.getElementsById('flips');
+    this.timer = document.getElementById('timeremaining');
+    this.ticker = document.getElementById('flips');
 
 
   }
@@ -13,16 +13,100 @@ class MixorMatch{
     this.totalClicks = 0;
     this.timeRemaining = this.totalTime;
     this.busy = true;
+
+    setTimeout(() => {
+      this.shuffleCards();
+      this.countDown = this.StartCountDown();
+      this.busy = false;
+    }, 500);
+    this.hideCards();
+    this.timer.innerText = this.timeRemaining;
+    this.ticker.innerText = this.totalCliks;
   }
 
-FlipCard(card) {
+  hideCards() {
+    this.cardsArray.forEach(card => {
+      card.classList.remove('visivle');
+      card.classList.remove('matched');
+    });
+  }
+
+  flipCard(card) {
+          if(this.canFlipCard(card)) {
+              this.totalClicks++;
+              this.ticker.innerText = this.totalClicks;
+              card.classList.add('visible');
+
+              if(this.cardToCheck) {
+                  this.checkForCardMatch(card);
+              } else {
+                  this.cardToCheck = card;
+              }
+          }
+      }
+      checkForCardMatch(card) {
+          if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+              this.cardMatch(card, this.cardToCheck);
+          else
+              this.cardMismatch(card, this.cardToCheck);
+
+          this.cardToCheck = null;
+      }
+      cardMatch(card1, card2) {
+    this.matchedCards.push(card1);
+    this.matchedCards.push(card2);
+    card1.classList.add('matched');
+    card2.classList.add('matched');
+    this.audioController.match();
+    if(this.matchedCards.length === this.cardsArray.length)
+        this.victory();
+      }
+      cardMismatch(card1, card2) {
+          this.busy = true;
+          setTimeout(() => {
+              card1.classList.remove('visible');
+              card2.classList.remove('visible');
+              this.busy = false;
+          }, 1000);
+      }
+
+
+getCardType(card) {
+  return card.getElementsByClassName('card-value')[0].src;
 
 }
 
-  canFlipCard(card) {
-    return true;
-    //return !this.busy && !this.matchedCards.includes(card) && !== this.cardToCheck;
+StartCountDown() {
+  return setInterval(() =>{
+    this.timeRemaining--;
+    this.timer.innerText = this.timeRemaining;
+    if(this.timeRemaining === 0)
+    this.gameOver();
+  }, 1000);
+}
+
+gameOver() {
+  clearInterval(this.countDown);
+  document.getElementById('game-over-text').classList.add('visible');
+}
+victory(){
+  clearInterval(this.countDown);
+  document.getElementById('victory-text').classList.add('visible');
+
+}
+
+shuffleCards() {
+  for(let i = this.cardsArray.length - 1; i > 0; i--) {
+    let randIndex = Math.floor(Math.random() * (i+1));
+    this.cardsArray[randIndex].style.order = i;
+    this.cardsArray[i].style.order = randIndex;
   }
+}
+
+canFlipCard(card) {
+  return true;
+     //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+ }
 }
 
 
@@ -30,17 +114,19 @@ FlipCard(card) {
 function ready() {
   let overlays = Array.from(document.getElementsByClassName('overlay-text'));
   let cards = Array.from(document.getElementsByClassName('card'));
+  let game = new MixorMatch(20, cards);
+
 
 
   overlays.forEach(overlay => {
     overlay.addEventListener('click', () =>{
       overlay.classList.remove('visible');
-      //game.startgame();
+      game.startGame();
     });
   });
   cards.forEach(card =>{
     card.addEventListener('click', () =>{
-      //game-flipCard(card);
+      game.flipCard(card);
     });
   });
 }
